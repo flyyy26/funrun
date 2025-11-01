ffae75fa91fb7fbee14ed36a60d315bacb9db4ca
flyyy26 <rfchannel0@gmail.com>
2025-11-01 15:39:16 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vasaka FUN RUN</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="banner">
+        <img src="images/vasaka_hotel.png" alt="" class="vasaka_hotel">
+        <img src="images/logo_banner.png" alt="" class="logo_banner">
+        <img src="images/tali_banner_kiri.svg" alt="" class="tali_banner_kiri">
+        <img src="images/tali_banner_kanan.svg" alt="" class="tali_banner_kanan">
+        <div class="button_banner">
+            <a href="#"><button>Register Now</button></a>
+            <a href="#"><button>See Event</button></a>
+        </div>
+    </div>
+</body>
+</html>
